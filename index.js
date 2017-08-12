02794d0ae3cead909e1511c65c3c1deaa0424972
Hunor Karamán <hunix94@gmail.com>
2017-08-12 10:31:16 +0300

    Code rebuild

--- index.js
+++ index.js
@@ -6,21 +6,11 @@ const fs = require('fs');
 
 const program = require("commander");
 const _version_ = require("./package.json").version;
+const Odot = require('./lib/odot.js');
 
 var json;
 
-var loadList = function(callback) {
-    var string = fs.readFile(".checklist.odot", "utf-8", function(err, data) {
-        if (err) {
-            json = {items: []}
-            fs.writeFileSync(".checklist.odot", JSON.stringify(json));
-            callback(json);
-        } else {
-            json = JSON.parse(data);
-            callback(json);
-        }
-    });
-}
+var odot = new Odot();
 
 program
     .version(_version_)
@@ -30,138 +20,50 @@ program
     .command("list").alias("=")
     .description("List your current list")
     .action(function() {
-        loadList(function (json) {
-            if (json.items.length == 0) console.log("Your list is empty, yet.");
-            else printList(json);
-        });
+        odot.equals();
 });
 
 program
     .command("add [item...]").alias("+")
     .description("Add an item to your list")
     .action(function(item) {
-        item = makeText(item);
-        loadList(function(json) {
-            json.items.push({"name": item, "done": false});
-            printList(json);
-            fs.writeFileSync(".checklist.odot", JSON.stringify(json));
-        });
+        item = Odot.makeText(item);
+        odot.plus(item);
 });
 
 program
     .command("remove [item...]").alias("-")
     .description("Remove an item to your list")
     .action(function(item) {
-        item = makeText(item);
-        loadList(function(json) {
-            let p = 0;
-            if (item == '') item = json.items.length;
-            if (isNaN(item)) {
-                json.items.forEach(function(element) {
-                    if (element.name == item) {
-                        json.items.splice(p, 1);
-                        return;
-                    }
-                    p++;
-                }, this);
-            } else {
-                var count = 0;
-                json.items.forEach(function(element) {
-                    count++;
-                    if (count == item) {
-                        json.items.splice(count - 1, 1);
-                        return;
-                    }
-                }, this);
-            }
-
-            printList(json);
-            fs.writeFileSync(".checklist.odot", JSON.stringify(json));
-        });
-
+        item = Odot.makeText(item);
+        odot.minus(item);
 });
 
 program
     .command("filter").alias("*")
     .description("Remove finished items from your list")
     .action(function() {
-        var res = {items: []}, c = 0;
-	    loadList(function (json) {
-            json.items.forEach(function (element) {
-		         if (!element.done) res.items[c++] = element;
-	        });
-
-	        printList(res);
-            fs.writeFileSync(".checklist.odot", JSON.stringify(res));
-        });
+        odot.filter();
 });
 
 program
     .command("check [item...]").alias("!")
     .description("Check an item on your list")
     .action(function(item) {
-        item = makeText(item);
-        loadList(function(json) {
-            if (item == '') {
-                // Find first unchecked
-                let counter = 1;
-                json.items.forEach(function(element) {
-                    if (element.done == false) {
-                        item = counter;
-                        return;
-                    }
-                    counter++;
-                }, this);
-            }
-            if (isNaN(item)) {
-                json.items.forEach(function(element) {
-                    if (element.name == item) {
-                        element.done = true;
-                        return;
-                    }
-                }, this);
-            } else {
-                let counter = 0;
-                json.items.forEach(function(element) {
-                    counter++;
-                    if (counter == item) {
-                        element.done = true;
-                        return;
-                    }
-                }, this);
-            }
-
-            printList(json);
-            fs.writeFileSync(".checklist.odot", JSON.stringify(json));
-        });
+        item = Odot.makeText(item);
+        odot.check(item);
 });
 
 program
     .command("clear").alias("0")
     .description("Clear your list")
     .action(function() {
-        console.log(colors.grey("Bye, bye! *o*"));
-        fs.unlink(".checklist.odot", function(err) {});
+        odot.zero();
 });
 
 program.parse(process.argv);
 
+// Output help by default
 if (!process.argv.slice(2).length) {
   program.outputHelp();
-}
-
-function printList(json) {
-    let counter = 1;
-    json.items.forEach(function(element) {
-        if (element.done) console.log(colors.green('✓ (' + counter++ + ') ' + element.name));
-        else console.log(colors.red.bold('✗ (' + counter++ + ') ' + element.name));
-    }, this);
-}
-
-function makeText(arr) {
-    var res = "";
-    arr.forEach(function (e) {
-        res += e + " ";
-    });
-    return res;
 }
